9775870bf58ea215a2ac27870e4f992ce02a9eb7
PofeiShit <191543154@qq.com>
2021-11-13 20:11:12 +0800

    add fig2_27 code

---
+++ ucl/test_case/test_file/test_fig2_27.c
@@ -0,0 +1,20 @@
+struct Data
+{
+    int num1;
+    int num2;
+} dt;
+enum Color 
+{
+    RED, GREEN, BLUE
+};
+typedef int INT32;
+int a, b, c, d;
+int main(int argc, char *argv[])
+{
+    char *hi = "Hello World.";
+    c = a + b;
+    d = a + b;
+    a = 3;
+    dt.num2 = a + b;
+    return 0;
+}
